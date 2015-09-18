9197a7c38f8daa34b51fc84c2818a4a6fa39df69
Ben Hong <bencodezen@gmail.com>
2015-09-18 17:53:40 -0400

    [F] Add my earning info calculations

--- app.js
+++ app.js
@@ -6,17 +6,29 @@ angular.module('staffCalcApp', [])
 		$scope.subtotal = 0;
 		$scope.tipAmount = 0;
 		$scope.total = 0;
+		$scope.tipTotal = 0;
+		$scope.mealCount = 0;
+		$scope.averageTip = 0;
 
 		$scope.calcSubtotal = function(basePrice, taxRate) {
-			$scope.subtotal = basePrice + (basePrice * (taxRate / 100));
+			return basePrice + (basePrice * taxRate);
 		};
+
 		$scope.calcTip = function(basePrice, tipPercent) {
-			$scope.tipAmount = basePrice * (tipPercent / 100);
+			return basePrice * tipPercent;
 		};
 
 		$scope.inputMeal = function() {
-		  $scope.calcSubtotal($scope.basePrice, $scope.taxRate);
-		  $scope.calcTip($scope.basePrice, $scope.taxRate);
-		  $scope.total = $scope.subtotal + $scope.tipAmount;
+		  var basePrice = $scope.basePrice;
+		  var taxPercent = $scope.taxRate / 100;
+		  var tipPercent = $scope.tipPercent / 100;
+		  var subtotal = $scope.calcSubtotal(basePrice, taxPercent);
+		  var tipAmount = $scope.calcTip(basePrice, tipPercent);
+		  $scope.subtotal = subtotal;
+		  $scope.tipAmount = tipAmount;
+		  $scope.total = subtotal + tipAmount;
+		  $scope.mealCount++;
+		  $scope.tipTotal += tipAmount;
+		  $scope.averageTip = $scope.tipTotal / $scope.mealCount;
 		};
 	});
